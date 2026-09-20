0e983a4695a0599734b203b93a0e5d8bf6aef44a
nupur185 <nupur56kumari7891@gmail.com>
2026-09-20 15:18:07 +0530

    Update App.jsx

--- krishiSarthi/frontend/src/App.jsx
+++ krishiSarthi/frontend/src/App.jsx
@@ -56,7 +56,7 @@ function App() {
 
         <Route
           path="/"
-          element={<Navigate to="/login" replace />}
+          element={<Navigate to="/register" replace />}
         />
 
       </Routes>
